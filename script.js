4ba03a2f4b2b834f9d31ce2f1d81006ff2075543
LinkGr <juangabrf@gmail.com>
2023-05-19 16:21:55 -0300

    m

---
+++ script.js
@@ -0,0 +1,8 @@
+
+
+let data = new Date();
+let hora = data.getHours().toString();
+let minutos = data.getMinutes().toString();
+let segundos = data.getSeconds().toString();
+
+console.log(hora + ':' + minutos + ':' + segundos);
